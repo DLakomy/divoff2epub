de724b0e82958c4993c7a700d4a6cc070da04de4
DLakomy <dawid.lakomy1@gmail.com>
2017-10-20 19:42:33 +0200

    Unlock e-book generation

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python divoff2md.py  > md/diurnale.md
-# not needed at the moment
-#pandoc --epub-chapter-level=2 --epub-stylesheet=style.css --epub-cover-image=img/cover.png --epub-metadata=meta.xml --toc-depth=2 -o mszalik.epub md/title.md md/mszalik.md
-#kindlegen mszalik.epub
-#open mszalik.epub
+pandoc --epub-chapter-level=2 --epub-stylesheet=style.css --epub-cover-image=img/cover.png --epub-metadata=meta.xml --toc-depth=2 -o diurnale.epub md/diurnale.md
+kindlegen diurnale.epub
+open diurnale.epub
